f322b8db35ff6ccf3ddad2a28ba153aef356eecc
cppsh1t <127290659+cppsh1t@users.noreply.github.com>
2023-05-28 16:07:03 +0800

    Add files via upload

--- MonoDelay.cs
+++ MonoDelay.cs
@@ -4,28 +4,42 @@ using UnityEngine;
 using System;
 using System.Linq;
 
-namespace MyTest.MonoDelay
+namespace MyTest.MonoTimer
 {
+    /// <summary>
+    /// 链接DelayTask状态的Token
+    /// </summary>
     public class DealyTaskToken
     {
         public bool canceled { get; private set; } = false;
-        public bool finished { get; private set; } = false;
 
+        /// <summary>
+        /// 取消DelayTask的执行
+        /// </summary>
         public void Cancel() => canceled = true;
-        public void Finish() => finished = true;
     }
 
-
+    /// <summary>
+    /// 延时任务
+    /// </summary>
     public class DealyTask : IComparable<DealyTask>
     {
         public Action delayCallback = () => { };
         public Action cancelCallback;
 
+        [Range(0, Single.MaxValue)]
         public float timeSpan;
         public float finalTime;
 
         public DealyTaskToken token = new DealyTaskToken();
 
+        /// <summary>
+        /// 构造一个延时任务
+        /// </summary>
+        /// <param name="timeSpan">等待事件</param>
+        /// <param name="delayCallback">回调</param>
+        /// <param name="cancelCallback">取消后的回调</param>
+        /// <param name="token">自定义Token</param>
         public DealyTask(float timeSpan, Action delayCallback, Action cancelCallback = null, DealyTaskToken token = null)
         {
             this.timeSpan = timeSpan;
@@ -43,12 +57,7 @@ namespace MyTest.MonoDelay
             return this.finalTime.CompareTo(other.finalTime);
         }
 
-        public static (DealyTask, DealyTaskToken) CreateWithToken(float timeSpan, Action delayCallback, Action cancelCallback = null)
-        {
-            DealyTask task = new DealyTask(timeSpan, delayCallback, cancelCallback);
-            return (task, task.token);
-        }
-
+        //内部初始化任务时间
         public void InitTask(bool ignoreTimeScale)
         {
             if (!ignoreTimeScale)
@@ -63,17 +72,37 @@ namespace MyTest.MonoDelay
         }
     }
 
-
+    /// <summary>
+    /// 延时系统
+    /// </summary>
     public class DelaySystem
-    {
+    {   
+        /// <summary>
+        /// 全局延时系统
+        /// </summary>
+        /// <returns></returns>
         public static DelaySystem GlobalDelay = new DelaySystem();
 
         private SortedSet<DealyTask> timeScaleSet = new SortedSet<DealyTask>();
         private SortedSet<DealyTask> ignoreTimeScaleSet = new SortedSet<DealyTask>();
 
+        private DeviationBox deviationBox = new DeviationBox();
+
+        /// <summary>
+        /// 是否有受TimeScale影响的任务在等待或执行
+        /// </summary>
+        /// <value></value>
         public bool timeScaleEmpty { get; private set; } = true;
+
+        /// <summary>
+        /// 是否有不受TimeScale影响的任务在等待或执行
+        /// </summary>
+        /// <value></value>
         public bool ignoreTimeScaleEmpty { get; private set; } = true;
 
+        /// <summary>
+        /// 当前任务总数
+        /// </summary>
         public int TaskCount => timeScaleSet.Count + ignoreTimeScaleSet.Count;
 
         public DelaySystem()
@@ -83,42 +112,51 @@ namespace MyTest.MonoDelay
             delayCore.delaySystem = this;
         }
 
+        /// <summary>
+        /// 添加一个延时任务
+        /// </summary>
+        /// <param name="task">延时任务</param>
+        /// <param name="ignoreTimeScale">是否忽略TimeScale</param>
         public void AddDelayTask(DealyTask task, bool ignoreTimeScale = false)
         {
             task.InitTask(ignoreTimeScale);
 
             if (!ignoreTimeScale)
             {
-                bool contain = timeScaleSet.TryGetValue(task, out var insideTask);
+                if (timeScaleSet.Contains(task))
+                {
+                    task.finalTime = deviationBox.GetDeviation(task.finalTime);
+                }
 
-                if (contain)
-                {
-                    insideTask.delayCallback += task.delayCallback;
-                }
-                else
-                {
-                    timeScaleSet.Add(task);
-                    timeScaleEmpty = false;
-                }
+                timeScaleSet.Add(task);
+                timeScaleEmpty = false;
             }
             else
             {
-                bool contain = ignoreTimeScaleSet.TryGetValue(task, out var insideTask);
+                if (ignoreTimeScaleSet.Contains(task))
+                {
+                    task.finalTime = deviationBox.GetDeviation(task.finalTime);
+                }
+                ignoreTimeScaleSet.Add(task);
+                ignoreTimeScaleEmpty = false;
 
-                if (contain)
-                {
-                    insideTask.delayCallback += task.delayCallback;
-                }
-                else
-                {
-                    ignoreTimeScaleSet.Add(task);
-                    ignoreTimeScaleEmpty = false;
-                }
             }
 
         }
 
-        public bool TryGetTimeScaleTask(out DealyTask task)
+        /// <summary>
+        /// 添加一个延时任务, 并返回其Token
+        /// </summary>
+        /// <param name="task">延时任务</param>
+        /// <param name="ignoreTimeScale">是否忽略TimeScale</param>
+        /// <returns>延时任务的Token</returns>
+        public DealyTaskToken AddDelayTaskWithToken(DealyTask task, bool ignoreTimeScale = false)
+        {
+            AddDelayTask(task, ignoreTimeScale);
+            return task.token;
+        }
+
+        private bool TryGetTimeScaleTask(out DealyTask task)
         {
             if (timeScaleEmpty)
             {
@@ -130,7 +168,7 @@ namespace MyTest.MonoDelay
             return true;
         }
 
-        public bool TryGetIgnoreTimeScaleTask(out DealyTask task)
+        private bool TryGetIgnoreTimeScaleTask(out DealyTask task)
         {
             if (ignoreTimeScaleEmpty)
             {
